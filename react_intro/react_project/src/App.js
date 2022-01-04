d43e65ed6e082e374d598464d4752f5296a54358
Kth <vcro12123@naver.com>
2022-01-04 20:29:47 +0900

    220104

---
+++ react_intro/react_project/src/App.js
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+
+function App() {
+  const [toDo,setTodo] = useState("");
+  const [toDos,setToDos] = useState([]);
+  const onChange = (e)=>setTodo( e.target.value);
+
+  const onSubmit = (e)=> {e.preventDefault();
+    if(toDo === ""){
+      return;
+    }
+    setToDos((currentArray) => [toDo,...currentArray]);
+    setTodo("");
+  }
+  console.log(toDos);
+    return (
+    
+    <div >
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+      <input value={toDo ||""} onChange={onChange} type='text' placeholder='Write your to do..' />
+      <button>Add to do</button>
+      </form>
+      <hr/>
+      <ul>
+
+      {toDos.map((item,index)=>(
+        <li key={index}>{item}</li>
+      ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
