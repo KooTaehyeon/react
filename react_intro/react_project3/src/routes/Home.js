c267c4396d813acd606467073dcc34884d876eef
Kth <vcro12123@naver.com>
2022-01-05 20:50:02 +0900

    re

--- react_intro/react_project3/src/routes/Home.js
+++ react_intro/react_project3/src/routes/Home.js
@@ -1,24 +1,26 @@
 import Movie from '../components/Movie';
 import { useEffect, useState } from 'react';
+import styles from "./Home.module.css";
 
 function Home (){
 
   const [loading,setLoading] = useState(true);
   const [movies,setMovies]= useState([]);
-  const getMovies= async()=>{// eslint-disable-line no-unused-vars
-    const json =  await (await fetch(`https:yts.mx/api/v2/list_movies.json?minimum_rating=8&sort_by=year`)
-      ).json();
-      setMovies(json.data.movies);
-      setLoading(false)
-    }; 
+
+  const getMovies = async () => {
+        const json = await (await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`)).json();
+        setMovies(json.data.movies);
+        setLoading(false);
+    };
     
   useEffect(()=>{
     getMovies();
     },[])
   console.log(movies);
   return (
-    <div>
-      {loading ? <h1>Loading...</h1>: <div>{movies.map(item =>
+    <div className={styles.container}>
+      {loading ? <h1>Loading...</h1>:  <div className={styles.movies}>
+        {movies.map(item =>
        <Movie
        key={item.id}
        id={item.id}
@@ -32,6 +34,6 @@ function Home (){
     </div>
   );
 
-  return
+  
 }
 export default Home;
