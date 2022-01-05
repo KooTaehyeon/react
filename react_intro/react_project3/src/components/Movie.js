9267b162ae7cdead8ac90e8566d8655a9be15bee
Kth <vcro12123@naver.com>
2022-01-05 16:12:30 +0900

    gh2

--- react_intro/react_project3/src/components/Movie.js
+++ react_intro/react_project3/src/components/Movie.js
@@ -5,7 +5,7 @@ function Movie({id,coverImage,title,summary,genres}){
  return <div>
  <img src={coverImage} alt='사진'></img>
 <h2> <Link to={`/movie/${id}`}> {title}</Link></h2>
-<p>{summary}</p>
+<p>{summary.length> 235? `${summary.slice(0,235)}`:summary} </p>
 <ul>
   {genres.map((g)=>(
     <li key={g}> {g}</li>
